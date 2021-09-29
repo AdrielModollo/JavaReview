de26241db17b072b8c5ef9eee441a2cb01c538e5
Adriel Modollo <adrielmodollo@outlook.com>
2021-09-28 22:10:07 -0300

    Tipos

--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -3,7 +3,9 @@ package tiposprimitivos;
 public class TiposPrimitivos {
 
 	public static void main(String[] args) {
+		String nome = "Adriel";
 		float nota = 8.5f;
 		System.out.println("A nota é: " + nota);
+		System.out.printf("A nota de %s é: %.2f \n" ,nome, nota); //Formata o campo por casas decimais
 	}
 }
