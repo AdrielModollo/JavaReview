8c936ca4355b149ec2dfc96bf82d4baecd35395b
Adriel Modollo <adrielmodollo@outlook.com>
2021-09-30 23:28:21 -0300

    Formas de atribuição

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -3,9 +3,8 @@ package operadoresaritmeticos;
 public class OperadoresAritmeticos {
 
 	public static void main(String[] args) {
-		int numero = 10;
-		int valor = 4 + numero--;
-		System.out.println(valor);
-		System.out.println(numero);
+		int x = 4;
+		x += 2; // x = x +2
+		System.out.println(x);
 	}
 }
