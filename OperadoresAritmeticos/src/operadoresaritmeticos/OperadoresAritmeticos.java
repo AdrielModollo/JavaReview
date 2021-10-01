fdf711efe49a79c5fbd0d572cd94cb6aee3bb0c7
Adriel Modollo <adrielmodollo@outlook.com>
2021-09-30 23:34:43 -0300

    Math

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -3,8 +3,25 @@ package operadoresaritmeticos;
 public class OperadoresAritmeticos {
 
 	public static void main(String[] args) {
-		int x = 4;
-		x += 2; // x = x +2
-		System.out.println(x);
+		/*
+		 * Arrendonda para baixo
+			float y = 8.9f;
+			int ar = (int) Math.floor(y);
+			System.out.println(ar);
+		*/
+		
+		/*
+		 * Arrendonda para baixo
+			float y = 8.9f;
+			int ar = (int) Math.cell(y);
+			System.out.println(ar);
+		*/
+		
+		/*
+		 * Arrendonda por aproximidade
+			float y = 8.9f;
+			int ar = (int) Math.round(y);
+			System.out.println(ar);
+		*/
 	}
 }
