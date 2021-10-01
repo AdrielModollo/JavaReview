f7b18a4254d5f8f501ca8a9b70d27540a681ac5b
Adriel Modollo <adrielmodollo@outlook.com>
2021-09-30 23:26:16 -0300

    Decremento

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -3,8 +3,9 @@ package operadoresaritmeticos;
 public class OperadoresAritmeticos {
 
 	public static void main(String[] args) {
-		int numero = 5;
-		int valor = 5 + numero++;
+		int numero = 10;
+		int valor = 4 + numero--;
 		System.out.println(valor);
+		System.out.println(numero);
 	}
 }
